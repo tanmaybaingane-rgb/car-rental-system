ced41807c1d1d4bf1759ea8063c10350a9cd159a
Tanmay Baingane <tanmaybaingane@gmail.com>
2026-06-05 19:08:47 +0530

    Remove database password from source

--- src/main/java/in/car/dbcon/DbConnection.java
+++ src/main/java/in/car/dbcon/DbConnection.java
@@ -6,22 +6,13 @@ import java.sql.DriverManager;
 public class DbConnection {
 
     private static final String URL =
-            System.getenv().getOrDefault(
-                    "DB_URL",
-                    "jdbc:mysql://localhost:3306/car_rental_db"
-            );
+            "jdbc:mysql://localhost:3306/car_rental_db";
 
     private static final String USER =
-            System.getenv().getOrDefault(
-                    "DB_USER",
-                    "root"
-            );
+            "root";
 
     private static final String PASSWORD =
-            System.getenv().getOrDefault(
-                    "DB_PASSWORD",
-                    ""
-            );
+            "";
 
     public static Connection getConnection() {
 
